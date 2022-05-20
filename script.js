61fa2e6d2e32712877adc1be4d8c944c11ff39e6
vkaash-sharma <vikasayushsharma@gmail.com>
2022-05-20 13:14:59 -0800

    :beers: project completed

--- script.js
+++ script.js
@@ -2,13 +2,18 @@ var uid = new ShortUniqueId();
 const addbtn = document.querySelector(".add-btn");
 const modalCont = document.querySelector(".modal-cont");
 const allPriorityColors =  document.querySelectorAll(".priority-color");
-let colors = ['lightpink','lightgreen','lightblue','black'];
+let colors = ["lightpink","lightgreen","lightblue","black"];
 let modalPriorityColor = colors[colors.length - 1];
 let textAreaCont = document.querySelector(".textarea-cont");
 const mainCont = document.querySelector(".main-cont");
 let ticketsArr = [];
 let toolBoxColors = document.querySelectorAll(".color");
 let removeBtn = document.querySelector(".remove-btn");
+
+
+let lockClass = "fa-lock";
+let unlockClass = "fa-lock-open";
+
 
 
 let isModalPresent = false;
@@ -19,10 +24,10 @@ addbtn.addEventListener("click", function() {
         modalCont.style.display = "none";
     }
     isModalPresent = !isModalPresent;
-})
+});
 
 
-
+//to remove and add active class from each priority color of modal container
 allPriorityColors.forEach(function (colorElement) {
     colorElement.addEventListener("click",function () {
         allPriorityColors.forEach(function (priorityColorElem){
@@ -36,8 +41,8 @@ allPriorityColors.forEach(function (colorElement) {
 modalCont.addEventListener("keydown" , function(e) {
     let key = e.key;
     if(key == "Shift"){
-        // console.log(modalPriorityColor);
-        // console.log(textAreaCont.value);
+        console.log(modalPriorityColor);
+        console.log(textAreaCont.value);
         createTicket(modalPriorityColor , textAreaCont.value);
         modalCont.style.display = "none";
         isModalPresent = false;
@@ -58,12 +63,15 @@ function createTicket(ticketColor , data , ticketId){
     <div class="ticket-color ${ticketColor} "></div>
     <div class="ticket-id">${id}</div>
     <div class="task-area">${data}</div>
-
+    <div class = "ticket-lock">
+       <i class="fa-solid fa-lock"></i>
+    </div>
     `;
     mainCont.appendChild(ticketCont);
 
     handleRemoval(ticketCont , id);
     handleColor(ticketCont , id);
+    handleLock(ticketCont,id);
    
     if(!ticketId){
         ticketsArr.push(
@@ -88,13 +96,13 @@ if(localStorage.getItem("tickets")){
 }
 
 
-
+//filter tickets on the basis of ticketColor
 for(let i = 0 ; i <toolBoxColors.length ; i++){
     toolBoxColors[i].addEventListener("click",function(){
         let currToolBoxColor = toolBoxColors[i].classList[0];
 
-        let filteredTickets =ticketsArr.filter(function(ticketObj){
-            return currToolBoxColor == ticketObj.ticketColor;
+        let filteredTickets = ticketsArr.filter(function(ticketObj){
+            if (currToolBoxColor == ticketObj.ticketColor) return ticketObj;
         });
 
         let allTickets = document.querySelectorAll(".ticket-cont");
@@ -175,8 +183,33 @@ function handleColor(ticket , id){
         ticketColorStrip.classList.remove(currTicketColor);
         ticketColorStrip.classList.add(newTicketColor);
 
-        let ticketIdx = getTicketIdx(idx);
+        let ticketIdx = getTicketIdx(id);
         ticketsArr[ticketIdx].ticketColor = newTicketColor;
         localStorage.setItem("tickets" , JSON.stringify(ticketsArr));
     });
+}
+//lock and unlock to make content editable true or false 
+function handleLock(ticket , id){
+    let ticketLockEle = ticket.querySelector(".ticket-lock");
+    let ticketLock = ticketLockEle.children[0];
+    let ticketTaskArea = ticket.querySelector(".task-area");
+     //toggle of icons and contenteditable property
+     ticketLock.addEventListener("click",function() {
+      let ticketIdx=getTicketIdx(id);
+      if(ticketLock.classList.contains(lockClass)){
+          ticketLock.classList.remove(lockClass);
+          ticketLock.classList.add(unlockClass);
+          ticketTaskArea.setAttribute("contenteditable","true");
+      }else{
+        ticketLock.classList.remove(unlockClass);
+        ticketLock.classList.add(lockClass);
+        ticketTaskArea.setAttribute("contenteditable", "false");
+      }
+
+      ticketTaskArea[ticketIdx].data = ticketTaskArea.innerText;
+      localStorage.setItem("tikcets",JSON.stringify(ticketsArr));
+
+     });
+
+
 }
